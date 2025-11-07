9430449937760065d3f91173a0ec928625ccde8c
aguslorenzoo <agustina.lorenzo@gmail.com>
2025-11-07 18:10:20 -0300

    arreglos

--- src/main.js
+++ src/main.js
@@ -5,19 +5,14 @@ import authRouter from "./routes/auth.router.js";
 import workspaceRouter from "./routes/workspace.router.js";
 import cors from 'cors'
 import memberRouter from "./routes/member.router.js";
-import MessagesChannelRepository from "./repositories/messageChannel.repository.js";
 
 
 connectToMongoDB()
 
 const app = express()
 
-//Configuro a mi API como API publica, cualquier dominio puede hacer peticiones
 app.use( cors() )
-
 app.use(express.json())
-
-//Todas las consultas que empiezen con /api/auth va a ser gestionadas por el authRouter
 app.use('/api/auth', authRouter)
 app.use('/api/workspace', workspaceRouter)
 app.use('/api/member', memberRouter)
